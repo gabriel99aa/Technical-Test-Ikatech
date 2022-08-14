8e89ba42bfafd00063517b87c905e05bc50d061d
Gabriel Agudelo <54853427+gabriel99aa@users.noreply.github.com>
2022-08-14 14:36:24 -0500

    Footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,60 @@
-import React from 'react'
+import React from "react";
+import {
+  IoLogoFacebook,
+  IoLogoInstagram,
+  IoIosArrowForward,
+} from "react-icons/io";
+
+import "./style.css";
 
 const Footer = () => {
   return (
     <>
-      
+      <div className="redes">
+        <h2>@IKATECH</h2>
+        <hr className="line" />
+        <div className="icons">
+          <IoLogoFacebook />
+          <IoLogoInstagram />
+        </div>
+      </div>
+      <div className="info">
+        <div className="servicio">
+          <h4>SERVICIO AL CLIENTE</h4>
+          <br />
+          <p>CONTACTENOS</p>
+          <p>CAMBIOS Y DEVOLUCIONES</p>
+          <p>POLÍTICAS DE LA TIENDA</p>
+          <p>POLITICA DE DATOS</p>
+        </div>
+        <div className="cuenta">
+          <h4>MICUENTA</h4>
+          <br />
+          <p>MIS PEDIDOS</p>
+          <p>MIS DEVOLUCIONES</p>
+        </div>
+        <div className="recursos">
+          <h4>RECURSOS</h4>
+          <br />
+          <p>TIENDAS</p>
+          <p>DEVOLUCIONES</p>
+        </div>
+        <div className="news">
+          <h4>NEWSLATTER</h4>
+          <br />
+          <p>registrate para ser el primero en recibir nuestras noticias</p>
+          <button className="email">
+            <div>
+              <p>E-MAIL</p>
+            </div>
+            <div className="next">
+              <IoIosArrowForward />
+            </div>
+          </button>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
